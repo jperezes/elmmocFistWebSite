3134015fcd4c52df93aed6c39a86043e7114dca1
jperezes <corleone82@gmail.com>
2016-01-18 06:20:45 -0500

    static web commit

---
+++ html/index.php
@@ -0,0 +1,46 @@
+<html style="height: 100%;">
+	<body style="height: 100%;">
+		<table border="0" style="height: 100%; width: 100%;">
+		  <tr>
+		      <td>
+		      	<img style="width: 50%; height: 50%; text-align: center; padding-left: 55px;" alt="" src="img/logo_01.png">
+		      </td>
+		      <td>
+		      	<img style="vertical-align: bottom; padding-top: 5px;" alt="" src="img/tabs_portada.png">
+		      </td>
+		  </tr>
+		  <tr height="1%">
+		      <td width="40%" style="padding-left: 30px; padding-right: 35px; padding-top: 15px; text-align: justify; vertical-align: top;">
+		      	<font color="#88888888">
+		      		<p><b>¿Qué puede hacer Melo por ti?</b></p>
+		      		<p>Haz que tus clientes se sientan como en casa disfrutando de la comodidad de desayunar en su habitación. 
+		      			Con un solo clic podrás hacer tu pedido, Sólo tendrás que dejarles nuestra MELO BOX a tus huéspedes cada mañana!</p>
+		      		<p>Calidad de producto, comodidad y personalización son los ingredientes principales que hacen única la propuesta de 
+		      			MELO CATERING.</p>
+		      		<p>¡Recuerda, sin salir de su habitación podrán tomar un desayuno completo y coger fuerzas para iniciar el día! 
+		      			Ya sean huéspedes por motivo de negocio o turismo, nosotros podemos complementar tu servicio ofreciendo la 
+		      			comodidad de desayunar tranquilamente mientras organizan su día, ¡Sé diferente al resto de hostales y conseguirás 
+		      			que tus huéspedes estén aún más contentos y vuelvan sin dudarlo!</p>
+					<p>Ponte en contacto con nosotros en info@melocatering.es e iremos en persona a entregarte una muestra gratis, 
+						para confiar en nuestra producto necesitas probarlo.</p>
+		      	</font>
+		      </td>
+		      <td>
+		      	<img style="width: 90%; height: 70%; padding-left: 20px;" alt="" src="img/portada.png">
+		      </td>
+		  </tr>
+		  <tr height="20%">
+		      <td colspan="2"  style="background-color: #953735; padding: 5px; text-align: center;">
+		      	<font color="#FFFFFF">
+		      		<p>
+		      			<b>Dirección:</b> C/ Independència, 102 Bajos 08902 Hospitalet de Llobregat. España | Email: info@melocatering.es | Website: http://melocatering.es
+		      			<br>
+		      			MELO CATERING, SL.
+		      		</p>
+		      		
+		      	</font>
+		      </td>
+		  </tr>
+		</table>
+	</body>
+</html>
